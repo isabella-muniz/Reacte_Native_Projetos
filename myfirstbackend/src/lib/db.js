14c1bdddab795346fd6284f6878f13788466a142
isabella-muniz <contato.isabellaramos@hotmail.com>
2025-02-20 17:13:45 +0000

    Enviar arquivos

--- myfirstbackend/src/lib/db.js
+++ myfirstbackend/src/lib/db.js
@@ -1,4 +1,4 @@
-import mysql from "mysql2/promise";
+/* import mysql from "mysql2/promise";
 
 // Criando um pool de conexões com o banco de dados MySQL
 // As variáveis de ambiente (process.env) são definidas no arquivo .env na raiz do projeto
@@ -21,6 +21,32 @@ const pool = mysql.createPool({
 // Funcção para obter uma conexão do pool
 export async function getConnection() {
     return await pool.getConnection();
+}
+
+export default pool;
+ */
+
+
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOSTNAME,
+  port: 3306, // Porta de comunicação correta
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000, // 10 segundos de timeout
+  /* ssl: {
+    ca: Buffer.from(process.env.DB_SSL_CA_BASE64, 'base64').toString('utf-8'), // Decodifica o certificado
+  }, */
+});
+
+// Função para obter uma conexão da pool
+export async function getConnection() {
+  return await pool.getConnection();
 }
 
 export default pool;
---
+++ myfirstbackend/src/lib/nocors.js
@@ -0,0 +1,28 @@
+/* export function allowCord(handler){
+    return async (req, res) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Credencials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorizathion")
+
+        if(req.method === "OPTIONS"){
+            return res.status(200).end();
+        }
+        return handler(req, res)
+    }
+} */
+
+    export function allowCors(handler) {
+        return async (req, res) => {
+          res.setHeader("Access-Control-Allow-Origin", "*");
+          res.setHeader("Access-Control-Allow-Credentials", "true");
+          res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE");
+          res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      
+          if (req.method === "OPTIONS") {
+            return res.status(200).end(); // Retorna 200 para todas as requisições OPTIONS
+          }
+      
+          return handler(req, res);
+        };
+      }
